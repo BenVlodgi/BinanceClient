3449f6f457f501cfb598752727193a1bc39a3efe
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2017-12-26 02:49:12 -0500

    Add Extension class with simplified dictionary retrieval

---
+++ ClientWPF/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binance.Net.ClientWPF
+{
+    public static class Extensions
+    {
+        public static TV GetValue<TK,TV>(this Dictionary<TK,TV> dictionary, TK key)
+        {
+            if (dictionary.ContainsKey(key))
+                return dictionary[key];
+            return default(TV);
+        }
+
+      
+    }
+}
