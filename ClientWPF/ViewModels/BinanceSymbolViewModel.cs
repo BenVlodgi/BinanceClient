66dfd446213b282379d3d4a23db51ac339993dce
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2017-12-26 02:50:27 -0500

    Update BinanceSymbolViewModel to have visible Asset and currency descriptors of tradingpair, and kline candle lists

--- ClientWPF/ViewModels/BinanceSymbolViewModel.cs
+++ ClientWPF/ViewModels/BinanceSymbolViewModel.cs
@@ -1,11 +1,56 @@
 ﻿using System.Collections.ObjectModel;
 using System.Linq;
 using Binance.Net.ClientWPF.MVVM;
+using Binance.Net.Objects;
+using System.Collections.Generic;
+using System;
+using System.Windows.Threading;
 
 namespace Binance.Net.ClientWPF.ViewModels
 {
-    public class BinanceSymbolViewModel: ObservableObject
+    public class BinanceSymbolViewModel : ObservableObject
     {
+
+        private void EnsureSymbolPairs()
+        {
+            if (symbolPair.Length == 0)
+            {
+                symbolPair = Global.SplitTradeSymbols(Symbol);
+                if (symbolPair.Length == 0)
+                {
+                    symbolPair = new string[] { "***", "***" };
+                }
+            }
+        }
+
+        private string[] symbolPair = new string[0];
+        public string SymbolAsset
+        {
+            get
+            {
+                EnsureSymbolPairs();
+                return symbolPair?[0] ?? "";
+            }
+        }
+
+        public string SymbolCurrency
+        {
+            get
+            {
+                EnsureSymbolPairs();
+                return symbolPair?[1] ?? "";
+            }
+        }
+
+        public string SymbolText
+        {
+            get
+            {
+                EnsureSymbolPairs();
+                return $"{symbolPair?[0]} | {symbolPair?[1]}";
+            }
+        }
+
         private string symbol;
         public string Symbol
         {
@@ -14,6 +59,11 @@ namespace Binance.Net.ClientWPF.ViewModels
             {
                 symbol = value;
                 RaisePropertyChangedEvent("Symbol");
+
+                symbolPair = new string[0];
+                RaisePropertyChangedEvent("SymbolText");
+                RaisePropertyChangedEvent("SymbolAsset");
+                RaisePropertyChangedEvent("SymbolCurrency");
             }
         }
 
@@ -93,7 +143,7 @@ namespace Binance.Net.ClientWPF.ViewModels
                 RaisePropertyChangedEvent("TradePrice");
             }
         }
-        
+
         private ObservableCollection<OrderViewModel> orders;
         public ObservableCollection<OrderViewModel> Orders
         {
@@ -104,6 +154,53 @@ namespace Binance.Net.ClientWPF.ViewModels
                 RaisePropertyChangedEvent("Orders");
             }
         }
+
+        private KlineInterval klineInterval = KlineInterval.OneHour;
+        public KlineInterval KlineInterval
+        {
+            get { return klineInterval; }
+            set
+            {
+                klineInterval = value;
+                RaisePropertyChangedEvent("KlineInterval");
+                RaisePropertyChangedEvent("Klines");
+            }
+        }
+
+        private Dictionary<KlineInterval, ObservableCollection<BinanceKline>> klines = Enum.GetValues(typeof(KlineInterval)).Cast<KlineInterval>().ToDictionary(val => val, val=> new ObservableCollection<BinanceKline>());
+        public ObservableCollection<BinanceKline> Klines
+        {
+            get
+            {
+                return klines[KlineInterval];
+            }
+            set
+            {
+                klines[KlineInterval] = value;
+                RaisePropertyChangedEvent("Klines");
+            }
+        }
+        public void AddKline(KlineInterval interval, BinanceKline klineValue)
+        {
+            klines[interval].Add(klineValue);
+
+            if (interval == klineInterval)
+                RaisePropertyChangedEvent("Klines");
+        }
+
+        public void AddKlines(KlineInterval interval, IEnumerable<BinanceKline> klineValues)
+        {
+            System.Windows.Application.Current.Dispatcher.Invoke(() =>
+            {
+                foreach (var kline in klineValues)
+                    //if(!klines[interval].contains)
+                    klines[interval].Add(kline);
+
+                if (interval == klineInterval)
+                    RaisePropertyChangedEvent("Klines");
+            });
+        }
+
 
         public BinanceSymbolViewModel(string symbol, double price)
         {
