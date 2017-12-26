1544d5d923b3db0d4253b19414d939316bbee26a
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2017-12-26 18:20:42 -0500

    Add StorageAttributte to be property identifier for db classes

---
+++ ClientWPF/StorageAttributes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binance.Net.ClientWPF
+{
+    class StorageAttributes
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true)]
+    public class StorageProperty : Attribute
+    {
+        public int PrimaryKeyLevel { get; set; } = -1;
+
+        public StorageProperty(int primaryKeyLevel = -1)
+        {
+            PrimaryKeyLevel = primaryKeyLevel;
+        }
+    }
+}
