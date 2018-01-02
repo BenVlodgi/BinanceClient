40945b780a99e70819af07e778af6514db70aee0
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2018-01-02 16:41:23 -0500

    Update MainWindow to have sorting functions

--- ClientWPF/MainWindow.xaml.cs
+++ ClientWPF/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using Binance.Net.ClientWPF.ViewModels;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 
 namespace Binance.Net.ClientWPF
 {
@@ -10,6 +15,45 @@ namespace Binance.Net.ClientWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void Ledger_Updated(object sender, System.Windows.Data.DataTransferEventArgs e)
+        {
+            if ((sender as ListView)?.View is GridView gridview)
+            {
+                foreach (var column in gridview.Columns)
+                {
+                    // Set the Width. Then clear it to cause the autosize.
+                    column.Width = 1;
+                    column.ClearValue(GridViewColumn.WidthProperty);
+                }
+            }
+        }
+
+        private GridViewColumnHeader listViewSortCol = null;
+        private SortAdorner listViewSortAdorner = null;
+
+        private void SortClick(object sender, RoutedEventArgs e)
+        {
+            var listView = sender as ListView;
+            var header = e.OriginalSource as GridViewColumnHeader;
+            string sortBy = header.Content.ToString();
+            header.Tag = header.Content;
+            if (listViewSortCol != null)
+            {
+                AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
+                listView.Items.SortDescriptions.Clear();
+            }
+        
+            ListSortDirection newDir = ListSortDirection.Ascending;
+            if (listViewSortCol == header && listViewSortAdorner.Direction == newDir)
+                newDir = ListSortDirection.Descending;
+        
+            listViewSortCol = header;
+            listViewSortAdorner = new SortAdorner(listViewSortCol, newDir);
+            AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
+            listView.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
         }
     }
 }
