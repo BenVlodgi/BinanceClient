0b9ba8eba6bc3bf8bd1ccd6067578071b0432356
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2018-01-08 16:47:05 -0500

    Update CoinMarket to make multiple attempts at getting data when timeout

--- ClientWPF/CoinMarket.cs
+++ ClientWPF/CoinMarket.cs
@@ -9,19 +9,60 @@ namespace Binance.Net.ClientWPF
 {
     public static class CoinMarket
     {
-        static ICoinmarketcapClient cmcc;
+        //static ICoinmarketcapClient cmcc;
         static public IEnumerable<Currency> Currencies { get; private set; }
 
 
         static CoinMarket()
         {
-            cmcc = new CoinmarketcapClient();
-            Currencies = cmcc.GetCurrencies(100000);
-            //TODO: This hangs on occasion
+            UpdateCurrencies();
+        }
+
+        static void UpdateCurrencies()
+        {
+            int attempts = 0;
+            TimeSpan attemptLength = default(TimeSpan);
+            IEnumerable<Currency> currencies = null;
+            do
+            {
+                attempts++;
+                var task = new Task(() =>
+                {
+                    try
+                    {
+                        DateTime start = DateTime.Now;
+                        ICoinmarketcapClient cmcc = new CoinmarketcapClient();
+                        currencies = cmcc.GetCurrencies(100000);
+                        DateTime end = DateTime.Now;
+                        attemptLength = end - start;
+                    }
+                    catch
+                    {
+
+                    }
+                });
+                task.Start();
+
+                //TODO: This hangs on occasion
+
+                task.Wait(1000);
+            } while (currencies == null);
+            Currencies = currencies;
+
+            if(attempts>1)
+            {
+                // This get annoying
+                //new MessageBox.MessageBoxService().ShowMessage($"CoinMarket Took {attempts} attempts to get the data. Success took {attemptLength.TotalSeconds} seconds.", "Coinmarket took multiple attempts to retrieve data.", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Exclamation);
+            }
         }
 
         static public Currency GetCurrency(string symbol)
         {
+            while(Currencies == null)
+            {
+                System.Threading.Thread.Sleep(10);
+            }
+
             symbol = Global.GetBinanceSymbolName(symbol);
 
             return Currencies.Where(currency => currency.Symbol == symbol).FirstOrDefault();
