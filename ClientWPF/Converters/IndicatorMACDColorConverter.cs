4c9970dd6e7a50f7efc16e1fed4a2f0950050fcb
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2018-01-20 06:24:49 -0500

    Add IndicatorMACDColorConverter

---
+++ ClientWPF/Converters/IndicatorMACDColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Binance.Net.ClientWPF
+{
+    public class IndicatorMACDColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var v = value as string;
+            if (v == null) return null;
+
+            if (v == "Growing") return Brushes.Green;
+            else if (v == "Losing") return Brushes.Red;
+            else return Brushes.Yellow;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
